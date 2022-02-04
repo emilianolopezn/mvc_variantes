e9d05c0b5827b5e8d3de6d2cbed5ac180d09c5e9
Ulsa <ulsa@ulsa.com>
2022-02-04 14:49:04 -0700

    Inicio

---
+++ resources/views/variantes/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de variantes Covid 19</title>
+</head>
+<body>
+    <h1>Lista de variantes Covid19</h1>
+    <p>*Actualizado al día 28 de enero</p>
+</body>
+</html>
